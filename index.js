bf54dc6d5052a7810d80a3551a10b8c393713921
StefannyRodrigues <stefanny.rodrigues564@gmail.com>
2024-05-08 16:31:55 -0300

    feat: create batalhas

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 
 
-
+// get herois 
 
 app.get('/herois', async (req, res) => {
     try {
@@ -48,7 +48,7 @@ app.get('/herois', async (req, res) => {
 
 
 
-
+// post herois
 
 app.post('/herois',async (req, res) => {
     try {
@@ -68,7 +68,7 @@ app.post('/herois',async (req, res) => {
 
 
 
-
+//delete herois
 
 app.delete('/herois/:id_heroi', async (req, res) => {
     try {
@@ -86,7 +86,7 @@ app.delete('/herois/:id_heroi', async (req, res) => {
 
 
 
-
+//put herois
 
 app.put('/herois/:id_heroi', async (req, res) => {
     try {
@@ -106,7 +106,7 @@ app.put('/herois/:id_heroi', async (req, res) => {
 
 
 
-
+// get id herois
 
 app.get('/herois/:id_heroi', async(req, res) => {
     try {
@@ -123,6 +123,75 @@ app.get('/herois/:id_heroi', async(req, res) => {
         res.status(500).send('Erro ao pegar heroi por ID');
     }
 });
+
+
+
+
+
+
+
+
+// get criar batalhas
+
+app.get('/batalhas/:heroi1_id/:heroi2_id', async (req, res) => {
+    
+    const { heroi1_id, heroi2_id } = req. params;
+    
+    try {
+        const vencedor_id = await vencedorCal(heroi1_id, heroi2_id)
+
+
+        await pool.query('INSERT INTO batalhas WHERE ( heroi1_id, heroi2_id, vencedor_id ) VALUES ($1, $2, $3)', [ heroi1_id, heroi2_id, vencedor_id])
+        const { rows } = await pool.query('SELECT * FROM herois WHERE id_heroi = $1', [vencedor_id]);
+        
+        res.json({
+            batalha: rows[0], message: 'batalha criada com sucesso'
+        })
+
+    } catch (error) {
+        console.error('Erro ao obter todos as batalhas!!!', error);
+        res.status(500).send('Erro ao obter as batalhas!');
+    }
+});
+
+//function
+async function vencedorCal(heroi01_id, heroi02_id) {
+    const heroi01 = await pool.query('SELECT * FROM  herois WHERE id_heroi = $1', [heroi01_id]);
+    const heroi02 = await pool.query('SELECT * FROM  herois WHERE id_heroi = $1', [heroi02_id]);
+
+    if (heroi01.rows[0].nivel > heroi02.rows[0].nivel) {
+        return heroi01_id;
+    } else if (heroi01.rows[0].nivel < heroi02.rows[0].nivel){
+        return heroi02_id;
+    } else {
+        
+        if (heroi01.rows[0].hp > heroi02.rows[0].hp) {
+            return heroi01_id;
+        } else if (heroi01.rows[0].hp < heroi02.rows[0].hp){
+            return heroi02_id;
+        }else {
+            return heroi01_id;
+        }
+    }
+}
+
+
+
+
+app.get('/batalhas', async (req, res) => {
+    try {
+        const resultado = await pool.query('SELECT * FROM batalhas');
+        res.json({
+            total: resultado.rowCount,
+            herois: resultado.rows,
+        });
+    } catch (error) {
+        console.error('Erro ao obter todos os herois', error);
+        res.status(500).send('Erro ao obter os herois');
+    }
+});
+
+
 
 
 
